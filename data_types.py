c70b709cc34a6e89aa3ba26c45d072cf60c23821
Dennis <dennisgurban44@gmail.com>
2025-09-13 02:41:13 +0400

    .gitignore updated.

---
+++ data_types.py
@@ -0,0 +1,19 @@
+# given_name is a variable that stores the
+# data 'Dennis'.
+given_name = 'Dennis'
+
+# Python's built-in print() function accepts
+# an argument and prints it to the screen.
+print(given_name)
+
+# Assign an integer to the variable no_of_apples.
+no_of_apples = 10
+
+# Float because of the decimal point.
+temperature = 30.5
+
+# True and False are boolean data types.
+has_a_car = False
+
+# NoneType data
+item = None
