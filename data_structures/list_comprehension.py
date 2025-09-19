7d5ae51b19e17b0a53b07ff76650ee699d38f3a8
Dennis <dennisgurban44@gmail.com>
2025-09-19 23:27:32 +0400

    list_comprehension.py example updated.

--- data_structures/list_comprehension.py
+++ data_structures/list_comprehension.py
@@ -1,9 +1,9 @@
-myList = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4, 5]
 
 # Iterate through each element inside
 # the myList list and square it.
 # Assign the result to myList.
 # This is an example of list comprehension.
-myList = [x ** 2 for x in myList]
+my_list = [x ** 2 for x in my_list]
 
-print('myList:', myList)
+print('my_list:', my_list)
