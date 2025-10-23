f26ece649445f859c41c4ca567c2f4a04fc2c582
Dennis <dennisgurban44@gmail.com>
2025-10-23 11:22:10 -0400

    Added an explanation for shallow copy in memory_location.py.

--- memory_location.py
+++ memory_location.py
@@ -1,11 +1,18 @@
-name_of_country = 'Liechtenstein'
+name_of_country = 'LLiechtenstein'
+
+# A new object is not being created.
+# Likewise, this does not create a shallow copy since
+# both variables still point to the same memory address.
+same_country = name_of_country
 
 # Display the memory location/address of the variable.
-print('\nhex(id(name_of_country)):', hex(id(name_of_country)))
+print('hex(id(name_of_country)):', hex(id(name_of_country)))
+
+print('\nhex(id(same_country)):', hex(id(same_country)))
 
 # Reveal the memory location/address of the string
 # literal/raw value.
-print('\nhex(id(\'Liechtenstein\')):', hex(id('Liechtenstein')))
+print('\nhex(id(\'LLiechtenstein\')):', hex(id('LLiechtenstein')))
 
 # Strings are immutable. Their contents can never change
 # after creation.
@@ -26,9 +33,49 @@ lst = [1, 2, 3]
 print(f"\nhex(id(lst)): {hex(id(lst))}")
 
 # Since the previous list object was assigned to a
-# variable, its memory address is remains saved
+# variable, its memory address remains saved
 # throughout the runtime of this program.
 
 # If a new list object is to be created, a new memory
 # address must be given to it.
 print(f"\nhex(id([1, 2, 3])): {hex(id([1, 2, 3]))}")
+
+# String slicing.
+# A new string object is being created because
+# strings are immutable in Python.
+name_of_country = name_of_country[1:]
+
+print('\nname_of_country:', name_of_country)
+
+print('\nhex(id(name_of_country)):', hex(id(name_of_country)))
+
+lst1 = [1, 2]
+
+# A new object and shallow copy is being created.
+lst2 = lst1[1:]
+
+# While a shallow copy will create a new memory
+# address for lst2, the indices in lst2 will still
+# point to the same memory addresses as in lst1,
+# unless a new value is assigned to them.
+
+print('\nhex(id(lst1)):', hex(id(lst1)))
+
+print('\nhex(id(lst2)):', hex(id(lst2)))
+
+print('\nlst1:', lst1)
+print('lst2:', lst2)
+
+print('\nhex(id(lst1[1])):', hex(id(lst1[1])))
+print('hex(id(lst2[0])):', hex(id(lst2[0])))
+
+lst2[0] = 10
+
+print('\nlst1:', lst1)
+print('lst2:', lst2)
+
+# Notice how the memory address that lst2[0]
+# references changes after a new integer is
+# assigned to it.
+print('\nhex(id(lst1[1])):', hex(id(lst1[1])))
+print('hex(id(lst2[0])):', hex(id(lst2[0])))
