163953f857711ef84fccdfa50404751592cd39c4
gurban-dev <dennisgurban44@gmail.com>
2026-03-23 00:00:18 -0400

    Updated long_lines.py file.

--- long_lines.py
+++ long_lines.py
@@ -1,34 +1,31 @@
 """
-In Python, there is a backslash (\) is
-called a line continuation character.
+In Python, there is a backslash (\) is called a line continuation
+character.
 
-It is used for breaking a long statement
-into multiple lines.
+It is used for breaking a long statement into multiple lines.
 
-Type the backslash character at the point you want
-to break the statement, then press the Enter key
-on the keyboard.
+Type the backslash character at the point you want to break the
+statement, then press the Enter key on the keyboard.
 """
 
 result1 = 2 * 2 + 3 * 3 + 4 * 4 + 5 * 5
 
 print(f'result1: {result1}')
 
-# The line continuation character at the end of the
-# first line tells the interpreter that the statement
-# is continued onto the next line.
+# The line continuation character at the end of the first line
+# tells the interpreter that the statement is continued onto
+# the next line.
 result2 = 2 * 2 + 3 * 3 + \
           4 * 4 + 5 * 5
 
 print(f'\nresult2: {result2}')
 
 """
-Including the line continuation character (\) in a print()
-statement will include the whitespace characters in the
-output.
+Including the line continuation character (\) in a print() statement
+will include the whitespace characters in the output.
 
-If I removed the newline character on the second line,
-whitespace characters would be seen in the output.
+If I removed the newline character on the second line, whitespace
+characters would be seen in the output.
 """
 print('\nToday it will be sunny with occasional showers. \
        \nThe temperature will reach seventy degrees.')
@@ -39,16 +36,13 @@ tuesday = 'appliances'
 
 wednesday = 'home\nmaintenance equipment'
 
-"""
-A statement that is enclosed in parentheses
-into multiple lines without using the line
-continuation character.
-"""
+# A statement that is enclosed in parentheses into multiple lines
+# without using the line continuation character.
 print("\nMonday's sales are ", monday,
       " and Tuesday's\nsales are ", tuesday,
       " and Wednesday's sales are ", wednesday, ".", sep='')
 
-# With f-string.
+# With an f-string.
 print(f"\nMonday's sales are {monday} "
       f"and Tuesday's\nsales are {tuesday} "
       f"and Wednesday's sales are {wednesday}.")
