d1031201e787b80e3aba097b36437977e2bdeb03
gurban-dev <dennisgurban44@gmail.com>
2026-03-22 21:57:26 -0400

    Arguments and parameters example updated.

--- functions/functions.py
+++ functions/functions.py
@@ -1,28 +1,25 @@
 '''
 Code Reusability
-Writing functions make it possible to write
-source code that performs a particular task
-and then reuse that code all throughout the
-program. These are referred to as user-defined
+Writing functions make it possible to write source code that
+performs a particular task and then reuse that code all
+throughout the program. These are referred to as user-defined
 functions.
 
-If there aren't any names between the open and closed
-parentheses, this indicates that the function does not
-accept any parameters.
+If there aren't any names between the open and closed parentheses,
+this indicates that the function does not accept any parameters.
 
 Syntax for a parameterless function:
 def function_name():
-  instruction_1
+	instruction_1
 
-  instruction_2
+	instruction_2
 
-  ...
+	...
 
-  etc.
+	etc.
 
-To invoke/call a declared function, type the
-name of the function followed by a pair of open
-and closed parentheses:
+To invoke/call a declared function, type the name of the function
+followed by a pair of open and closed parentheses:
 
 function_name()
 '''
@@ -30,21 +27,28 @@ function_name()
 # Function without parameters.
 # This is a void function because it returns None.
 
-# The function say_hello() prints a message but does
-# not send a value back to where it was invoked in the
-# program.
-def say_hello(no_of_orders: int) -> None:
-  print("Hello, World!")
+# The function compute_order() prints a message but does not send
+# a value back to where it was invoked in the program.
+def compute_order(no_of_orders):
+	print("no_of_orders:", no_of_orders)
 
-  total: int = 10 * no_of_orders
+	total = 10 * no_of_orders
 
-  print('total:', total)
+	print('total:', total)
 
-  # Have the function return a value if you need to
-  # access that value outside of the function.
-  # return total
+	# Have the function return a value if you need to
+	# access that value outside of the function.
+	# return total
 
-return_val: int | None = say_hello(5)
+	# The default behaviour of a user-defined function
+	# is to return None unless specified otherwise.
+	# return None
+
+
+# The argument is always passed during the line where the
+# function call takes place and is put in between the
+# parentheses.
+return_val = compute_order(5)
 
 # Output:
 # return_val: 50
@@ -60,77 +64,75 @@ are written.
 
 Syntax for a parameterised function:
 def function_name(parameter_1, parameter_2):
-  instruction_1
+	instruction_1
 
-  instruction_2
+	instruction_2
 
-  ...
+	...
+
+    etc.
 
 function_name(argument_1, argument_2)
 
-When function_name() is invoked/called, argument_1
-will be assigned to parameter_1 just as argument_2
-will be assigned parameter_2.
-
-argument_1 and argument_2 are being passed to
-function_name().
-
-function_name() is accepting parameter_1 and
+When function_name() is invoked/called, argument_1 will be
+assigned to parameter_1 just as argument_2 will be assigned
 parameter_2.
+
+argument_1 and argument_2 are being passed to function_name().
+
+function_name() is accepting parameter_1 and parameter_2.
 '''
 
 '''
-A parameter can be thought of as a piece of
-data that a function will accept.
+A parameter can be thought of as a piece of data that a
+function will accept.
 
-The add() function is accepting two pieces
-of data called "first_num" and "second_num".
+The add() function is accepting two pieces of data called
+"first_num" and "second_num".
 
-To determine where the data is coming from,
-analyze the function call for add().
+To determine where the data is coming from, analyze the
+function call for add().
 
-The value of the first argument which is "num1",
-is assigned to the first parameter, "first_num".
+The value of the first argument which is "num1", is assigned
+to the first parameter, "first_num".
 '''
 
 # Function with parameters.
-def add(first_num: int, second_num: int) -> int:
-  print('\nfirst_num + second_num:', first_num + second_num)
+def add(first_num, second_num):
+	print('\nfirst_num + second_num:', first_num + second_num)
 
-  # return first_num + second_num
+	# return first_num + second_num
 
 # Two integer variables declared.
-num1: int = 3
-num2: int = 5
+num1 = 3
+num2 = 5
 
 '''
 Pass the arguments to the add() function.
-It's important to note that the names of the
-arguments do not have to be the same as the
-parameter names that can be seen in the
-function signature.
+It's important to note that the names of the arguments do not
+have to be the same as the parameter names that can be seen
+in the function signature.
 
-num1 will be assigned to parameter "first_num"
-and num2 will be assigned to parameter
-"second_num" because of the order that these
-arguments were passed to the add() function
-in.'''
+num1 will be assigned to parameter "first_num" and num2 will
+be assigned to parameter "second_num" because of the order
+that these arguments were passed to the add() function in.
+'''
 
 # Output: 8
 print('add(num1, num2):', add(num1, num2))
 
 num1 = 1
 num2 = 2
+
 add(num1, num2)
 
-# What does the subsequent line output in the
-# terminal?
+# What does the subsequent line output in the terminal?
 add(3, 5)
 
 '''
-The output "8" is not seen anymore in the
-terminal because the function call add(3, 5)
-is not inside of a print statement.
+The output "8" is not seen anymore in the terminal because
+the function call add(3, 5) is not inside of a print statement.
 
-If nothing is printed out with print(), nothing
-will be outputted to the terminal.'''
+If nothing is printed out with print(), nothing will be outputted
+to the terminal.
+'''
