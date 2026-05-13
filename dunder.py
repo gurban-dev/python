121efb7f75c38728347f79a57392cbad53b54542
gurban-dev <dennisgurban44@gmail.com>
2026-05-13 02:36:36 -0400

    Pandas type conversion chapter introduced.

--- dunder.py
+++ dunder.py
@@ -4,9 +4,10 @@ Dunder methods and variables in Python
 The term "dunder" is short for "double underscore".
 ---------------------------------------------------------
 
-Dunder methods and variables are predefined names that let your objects define
-exactly what happens when they are used with Python's built-in operations
-(like +, print(), ==, <, >) and constructs (like for-loops and len()).
+Dunder methods and variables are predefined names that let your
+objects define exactly what happens when they are used with
+Python's built-in operations (like +, print(), ==, <, >) and
+constructs (like for-loops and len()).
 
 Dunder Methods (behaviour)
 --------------------------
@@ -70,10 +71,15 @@ class Country:
 	def __str__(self):
 		# return f"\n{self.country_name}"
 		return "\n" + self.country_name
-	
+
+	# Determines the way a list of Country objects would be
+	# displayed when printing the list.
 	def __repr__(self):
 		return self.country_name
 	
+	# Determines how two different Country objects are compared.
+	# Think about what attributes should be compared for this class
+	# when deciding if two different Country objects are equal.
 	def __eq__(self, other_obj):
 		# Check if the other object is not an instance of this class.
 
@@ -93,17 +99,17 @@ countries = [luxembourg1, luxembourg2]
 
 print(f'luxembourg1: {luxembourg1}')
 
+# The text that is displayed when printing out the content of a list
+# of objects is controlled by the .__repr__() dunder method.
 print(f'\ncountries: {countries}')
 
-
-print(f'\nluxembourg1 == luxembourg2: {luxembourg1 == luxembourg2}')
-
-# The above comparison with the equality operator is equivalent to:
+# The below comparison with the equality operator is equivalent to:
 # luxembourg1.__eq__(luxembourg2)
 
-# In the __eq__() dunder method:
+# In the .__eq__() dunder method:
 # self references 'luxembourg1'.
 # other_obj references 'luxembourg2'.
+print(f'\nluxembourg1 == luxembourg2: {luxembourg1 == luxembourg2}')
 
 # The __class__ dunder variable reveals the name of the class
 # that the object is an instance of.
--- exercises/conditions_ex.py
+++ exercises/conditions_ex.py
@@ -1,6 +1,7 @@
 '''
 Task 1:
 Have the user type in their favourite season (E.g. summer).
+
 Write an if-elif-else statement that accurately prints what the user
 chose.
 
@@ -11,7 +12,7 @@ Practice comparing strings and using .lower() for case-insensitive matching.
 
 Case-insensitive means that the case of the letters doesn't matter.
 
-Understand that only one block in an if / elif / else chain runs.
+Understand that only one block in an if / elif / else statement runs.
 
 ----------------------------------------------------------------------------
 
@@ -33,3 +34,36 @@ affecting each other.
 Understand that both, one, or none of the messages can print depending
 on the inputs.
 '''
+
+# 1. Have the user type in their favourite season.
+# 2. Use an if-elif-else statement to display whether the user
+#    chose "spring", "summer", "autumn" or "winter".
+
+# The input() function:
+# • Pauses the program temporarily
+# • Waits until the user clicks the 'Enter' button on their keyboard
+# • Returns what the user typed in.
+
+# 'favourite_season' is the variable.
+
+# The value being assigned to the variable is the string
+# that is returned by the  function.
+favourite_season = input("What is your favourite season? ")
+
+print("\nfavourite_season:", favourite_season, '\n')
+
+if favourite_season == 'spring':
+    print("You inputted \"spring\".")
+
+elif favourite_season == 'summer':
+    print("You inputted \"summer\".")
+
+elif favourite_season == 'autumn':
+    print("You inputted \"autumn\".")
+
+elif favourite_season == 'winter':
+    print("You inputted \"winter\".")
+
+else:
+    print("Favourite season can only be \"winter\", \"spring\", "
+          "\"summer\" and \"autumn\".")
